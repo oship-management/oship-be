fe98192481931b917cb1e8f2d5c64e25f8470263
mxcoogi <mxcoogi@gmail.com>
2025-06-01 21:10:34 +0900

    ✅[test] cicd test

--- src/main/java/org/example/oshipserver/global/common/controller/HealthCheckController.java
+++ src/main/java/org/example/oshipserver/global/common/controller/HealthCheckController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
     private final String uuid = String.valueOf(UUID.randomUUID());
+
     @GetMapping("/health")
     public String healthCheck() {
         return "OK-SERVER-" + uuid;
