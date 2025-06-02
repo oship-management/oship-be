63be0197cbf84b356ac788018bb2e356832d00db
yeeun0702 <kimyeeun0702@naver.com>
2025-06-02 19:20:06 +0900

    ✨ [feat] #11 주문 생성 비즈니스 로직 및 컨트롤러 구현

---
+++ src/main/java/org/example/oshipserver/domain/order/controller/OrderController.java
@@ -0,0 +1,28 @@
+package org.example.oshipserver.domain.order.controller;
+
+import jakarta.validation.Valid;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.example.oshipserver.domain.order.dto.request.OrderCreateRequest;
+import org.example.oshipserver.domain.order.service.OrderService;
+import org.example.oshipserver.global.common.response.BaseResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public BaseResponse<Map<String, String>> createOrder(@Valid @RequestBody OrderCreateRequest orderCreateRequest) {
+        String masterNo = orderService.createOrder(orderCreateRequest);
+        return new BaseResponse<>(200, "주문 생성이 완료되었습니다.", Map.of("oship_master_no", masterNo));
+    }
+
+
+}
---
+++ src/main/java/org/example/oshipserver/domain/order/service/OrderService.java
@@ -0,0 +1,94 @@
+package org.example.oshipserver.domain.order.service;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.example.oshipserver.domain.order.dto.request.OrderCreateRequest;
+import org.example.oshipserver.domain.order.entity.Order;
+import org.example.oshipserver.domain.order.entity.OrderItem;
+import org.example.oshipserver.domain.order.entity.OrderRecipient;
+import org.example.oshipserver.domain.order.entity.OrderSender;
+import org.example.oshipserver.domain.order.entity.RecipientAddress;
+import org.example.oshipserver.domain.order.entity.SenderAddress;
+import org.example.oshipserver.domain.order.repository.OrderRepository;
+import org.example.oshipserver.global.exception.ApiException;
+import org.example.oshipserver.global.exception.ErrorType;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public String createOrder(OrderCreateRequest orderCreateRequest) {
+
+        // 중복 주문 확인 (주문번호 기준)
+        if (orderRepository.existsByOrderNo(orderCreateRequest.orderNo())) {
+            throw new ApiException("이미 동일한 주문번호가 존재합니다: " + orderCreateRequest.orderNo(), ErrorType.DUPLICATED_ORDER);
+        }
+
+        // Master No 생성
+        String masterNo = UUID.randomUUID().toString();
+        // 1. 주문 생성
+        Order order = Order.of(orderCreateRequest, masterNo);
+
+        // 2. 아이템 생성
+        List<OrderItem> items = orderCreateRequest.orderItems().stream()
+            .map(OrderItem::of)
+            .toList();
+        order.addItems(items);
+
+        // 3. 발송자 주소 생성
+        SenderAddress senderAddress = SenderAddress.builder()
+            .senderCountryCode(orderCreateRequest.senderCountryCode())
+            .senderState(orderCreateRequest.senderState())
+            .senderStateCode(orderCreateRequest.senderStateCode())
+            .senderCity(orderCreateRequest.senderCity())
+            .senderAddress1(orderCreateRequest.senderAddress1())
+            .senderAddress2(orderCreateRequest.senderAddress2())
+            .senderZipCode(orderCreateRequest.senderZipCode())
+            .senderTaxId(orderCreateRequest.senderTaxId())
+            .build();
+
+        // 4. 발송자 생성
+        OrderSender sender = OrderSender.builder()
+            .storePlatform(orderCreateRequest.storePlatform())
+            .storeName(orderCreateRequest.storeName())
+            .senderName(orderCreateRequest.senderName())
+            .senderEmail(orderCreateRequest.senderEmail())
+            .senderPhoneNo(orderCreateRequest.senderPhoneNo())
+            .address(senderAddress)
+            .build();
+        sender.assignOrder(order);
+        order.assignSender(sender);
+
+        // 5. 수취인 주소 생성
+        RecipientAddress recipientAddress = RecipientAddress.builder()
+            .recipientCountryCode(orderCreateRequest.recipientCountryCode())
+            .recipientState(orderCreateRequest.recipientState())
+            .recipientStateCode(orderCreateRequest.recipientStateCode())
+            .recipientCity(orderCreateRequest.recipientCity())
+            .recipientAddress1(orderCreateRequest.recipientAddress1())
+            .recipientAddress2(orderCreateRequest.recipientAddress2())
+            .recipientZipCode(orderCreateRequest.recipientZipCode())
+            .recipientTaxId(orderCreateRequest.recipientTaxId())
+            .build();
+
+        // 6. 수취인 생성
+        OrderRecipient recipient = OrderRecipient.builder()
+            .recipientName(orderCreateRequest.recipientName())
+            .recipientCompany(orderCreateRequest.recipientCompany())
+            .recipientEmail(orderCreateRequest.recipientEmail())
+            .recipientPhoneNo(orderCreateRequest.recipientPhoneNo())
+            .recipientAddress(recipientAddress)
+            .build();
+        recipient.assignOrder(order);
+        order.assignRecipient(recipient);
+
+        // 7. 저장
+        orderRepository.save(order);
+        return masterNo;
+    }
+}
+
+
