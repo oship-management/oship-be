30f8d95f566e33a9d9b5db8793b45da01d5344de
yeeun0702 <kimyeeun0702@naver.com>
2025-06-02 11:53:47 +0900

    ✨ [feat] #5 OrderRecipient, OrderSender 엔티티 구현

---
+++ src/main/java/org/example/oshipserver/domain/order/entity/OrderSender.java
@@ -0,0 +1,42 @@
+package org.example.oshipserver.domain.order.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "senders")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+class OrderSender {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 추후 seller와의 연관관계 매핑
+    private Long sellerId;
+
+    private String storePlatform;
+    private String storeName;
+    private String senderName;
+    private String senderEmail;
+    private String senderPhoneNo;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "sender_address_id")
+    private SenderAddress address;
+}
