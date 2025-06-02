20e689b8de4bb6dbb28f5117e6a3feb93a50db6d
yeeun0702 <kimyeeun0702@naver.com>
2025-06-02 16:03:08 +0900

    ✨ [feat] #5 OrderRecipient, OrderSender 칼럼명 추가

--- src/main/java/org/example/oshipserver/domain/order/entity/OrderRecipient.java
+++ src/main/java/org/example/oshipserver/domain/order/entity/OrderRecipient.java
@@ -1,17 +1,18 @@
 package org.example.oshipserver.domain.order.entity;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.Id;
 
 @Entity
 @Table(name = "order_recipients")
@@ -23,9 +24,16 @@ class OrderRecipient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String recipientName;
+
+    @Column(name = "company")
     private String recipientCompany;
+
+    @Column(name = "email")
     private String recipientEmail;
+
+    @Column(name = "phone_no")
     private String recipientPhoneNo;
 
     @OneToOne(fetch = FetchType.LAZY)
