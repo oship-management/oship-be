5a57801e3fafffee7779253e084a731e7fabee5d
dodu232 <dodu7510@gmail.com>
2025-06-03 16:33:40 +0900

    :sparkles: [feat] Carrier 관련 entity 추가
    - .gitkeep 삭제
    - carriers
    - carrier_countries
    - carrier_rate_cahrges

---
+++ src/main/java/org/example/oshipserver/domain/carrier/entity/CarrierRateCharge.java
@@ -0,0 +1,39 @@
+package org.example.oshipserver.domain.carrier.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.oshipserver.global.entity.BaseTimeEntity;
+
+@Entity
+@Table(name = "carrier_rate_charges")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CarrierRateCharge extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "carrier_id")
+    private Carrier carrier;
+
+    @Column(nullable = false)
+    private int zoneIndex;
+
+    @Column(nullable = false)
+    private BigDecimal weight;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+}
