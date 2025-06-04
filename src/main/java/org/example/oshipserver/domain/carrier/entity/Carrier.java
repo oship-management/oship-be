6abfa4a14d9593f2a02f1c07030b4d4a64dc3dd5
dodu232 <dodu7510@gmail.com>
2025-06-04 10:23:45 +0900

    :sparkles:  [feat] CarrierName추가 및 Carrier 엔티티에 적용

--- src/main/java/org/example/oshipserver/domain/carrier/entity/Carrier.java
+++ src/main/java/org/example/oshipserver/domain/carrier/entity/Carrier.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.oshipserver.domain.carrier.enums.CarrierName;
 import org.example.oshipserver.domain.carrier.enums.Services;
 import org.example.oshipserver.domain.partner.entity.Partner;
 import org.example.oshipserver.global.entity.BaseTimeEntity;
@@ -33,7 +34,8 @@ public class Carrier extends BaseTimeEntity {
     private Partner partner;
 
     @Column(length = 255, nullable = false)
-    private String name;
+    @Enumerated(value = EnumType.STRING)
+    private CarrierName name;
 
     @Column(length = 255)
     private String description;
