17848e216eaf97dd03271f79b03fba33e1399190
dodu232 <dodu7510@gmail.com>
2025-06-04 10:20:21 +0900

    :recycle: [refactor] ManyToOne fetch 전략 LAZY로 변경

--- src/main/java/org/example/oshipserver/domain/carrier/entity/CarrierRateCharge.java
+++ src/main/java/org/example/oshipserver/domain/carrier/entity/CarrierRateCharge.java
@@ -2,6 +2,7 @@ package org.example.oshipserver.domain.carrier.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,7 +25,7 @@ public class CarrierRateCharge extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "carrier_id")
     private Carrier carrier;
 
