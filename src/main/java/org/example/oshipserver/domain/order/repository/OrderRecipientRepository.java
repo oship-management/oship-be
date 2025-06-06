626919c02fe28c585ba1e510abf69232d2c7d939
dodu232 <dodu7510@gmail.com>
2025-06-06 17:17:34 +0900

    :sparkles:  [feat] order에서 무게, 국가 코드 가져오는 기능 추가

--- src/main/java/org/example/oshipserver/domain/order/repository/OrderRecipientRepository.java
+++ src/main/java/org/example/oshipserver/domain/order/repository/OrderRecipientRepository.java
@@ -1,5 +1,6 @@
 package org.example.oshipserver.domain.order.repository;
 
+import java.util.Optional;
 import org.example.oshipserver.domain.order.entity.OrderRecipient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRecipientRepository extends JpaRepository<OrderRecipient, Long> {
 
+    Optional<OrderRecipient> findByOrderId(Long orderId);
 }
