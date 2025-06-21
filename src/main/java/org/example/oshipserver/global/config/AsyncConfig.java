9d969e1d41e775abeec8eae40ff12460f2d06105
yeeun0702 <kimyeeun0702@naver.com>
2025-06-21 20:06:20 +0900

    ✨ [feat] #144 동기 및 비동기 메일 전송 구조 도입

---
+++ src/main/java/org/example/oshipserver/global/config/AsyncConfig.java
@@ -0,0 +1,16 @@
+package org.example.oshipserver.global.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean
+    public Executor taskExecutor() {
+        return Executors.newCachedThreadPool();
+    }
+}
