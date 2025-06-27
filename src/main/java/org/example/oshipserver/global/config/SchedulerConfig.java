7294ee9eb34e431c157f4fe18a547def7f26cbda
DESKTOP-M7S61C8\박수박 <pospara2@gmail.com>
2025-06-28 00:20:21 +0900

    ♻️ [fix] #162 오탈자 및 pull dev

---
+++ src/main/java/org/example/oshipserver/global/config/SchedulerConfig.java
@@ -0,0 +1,22 @@
+package org.example.oshipserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+    
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.setThreadNamePrefix("scheduled-task-");
+        threadPoolTaskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+        threadPoolTaskScheduler.setAwaitTerminationSeconds(60);
+        threadPoolTaskScheduler.initialize();
+        
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
