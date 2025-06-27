111fa1f5067cff5873d5798ab399e569aa2355c4
dodu232 <dodu7510@gmail.com>
2025-06-27 19:10:52 +0900

    :recycle:  [refactor] 요율 등록 로직 수정

--- src/main/java/org/example/oshipserver/domain/admin/dto/request/RateExcelRequest.java
+++ src/main/java/org/example/oshipserver/domain/admin/dto/request/RateExcelRequest.java
@@ -1,11 +1,13 @@
 package org.example.oshipserver.domain.admin.dto.request;
 
+import java.util.Map;
+import lombok.Builder;
+
+@Builder
 public record RateExcelRequest(
     int index,
-    Long carrierId,
-    Integer zoneIndex,
     Double weight,
-    Double amount
+    Map<Integer, Double> amounts
 ) {
 
 }
---
+++ src/main/java/org/example/oshipserver/domain/admin/service/RateExcelProcessor.java
@@ -0,0 +1,34 @@
+package org.example.oshipserver.domain.admin.service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.example.oshipserver.domain.admin.dto.request.RateCreateRequest;
+import org.example.oshipserver.domain.admin.dto.request.RateExcelRequest;
+import org.example.oshipserver.global.common.excel.AbstractExcelUploadProcessor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RateExcelProcessor extends
+    AbstractExcelUploadProcessor<RateExcelRequest, RateCreateRequest> {
+
+    public RateExcelProcessor(RateExcelParser parser) {
+        super(
+            parser, 1);
+    }
+
+    @Override
+    protected RateCreateRequest processRecord(RateExcelRequest dto) {
+        Map<Integer, BigDecimal> bigAmounts = dto.amounts().entrySet().stream()
+            .collect(Collectors.toMap(
+                Map.Entry::getKey,
+                e -> BigDecimal.valueOf(e.getValue())
+            ));
+
+        return RateCreateRequest.builder()
+            .index(dto.index())
+            .weight(BigDecimal.valueOf(dto.weight()))
+            .amounts(bigAmounts)
+            .build();
+    }
+}
