de87b9c67cc260c5e08bfa5fc2c4d541a512374f
yeeun0702 <kimyeeun0702@naver.com>
2025-07-03 12:19:58 +0900

    ✨ [feat] #178 이메일 전송 실패 시 재시도 및 Dead Letter Queue(DLQ) 처리 기능 추가

--- src/main/java/org/example/oshipserver/domain/notification/service/sender/GmailSmtpEmailSender.java
+++ src/main/java/org/example/oshipserver/domain/notification/service/sender/GmailSmtpEmailSender.java
@@ -1,4 +1,4 @@
-package org.example.oshipserver.domain.notification.service;
+package org.example.oshipserver.domain.notification.service.sender;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
